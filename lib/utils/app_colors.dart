74eb46c7f48a145b3a6bc3d6148cb21895585c91
Muhammad Hassan <immaharhasaan@gmail.com>
2025-10-30 13:46:51 +0500

    Update message

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF007AFF);
+  static const Color secondary = Color(0xFF00C853);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color textDark = Color(0xFF212121);
+  static const Color textLight = Color(0xFF757575);
+  static const Color error = Color(0xFFD32F2F);
+}
